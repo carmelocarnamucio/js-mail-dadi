eceec9dfc41a63e36959778b090b89faa557c40b
Carmelo Carnamucio <carnamuciocarmelo@gmail.com>
2020-10-22 16:36:55 +0200

    aggiunto html e css

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,23 +4,25 @@
 
 // variabili
 var richiestaEmail = prompt("Inserici la tua Email");
+// array, lista email presenti in archivio
 var emailUtenti = ["pinco@gmail.com", "pallino@gmail.com", "ciao@gmail.com", "arrivederci@gmail.com", "addio@gmail.com", "ciaone@gmail.com"];
+// variabile di controllo
 var convalida = false;
 
-
+// verifico che la mail inserita sia presente
 for (var i = 0; i < emailUtenti.length; i++) {
 
-  // email in lista
+// condizione: La mail inserita dall'utente deve corrispondere ad una presente nell'elenco
+  // se la mail inserita è presente
   if (richiestaEmail === emailUtenti[i]) {
     convalida = true;
     document.getElementById("Risultato").innerHTML = ("sei autorizzato")
   }
-
 }
 
-console.log(convalida);
+//console.log(convalida);
 
-// se email non in lista
+// se la mail inserita non è presente
 if (convalida == false) {
   document.getElementById("Risultato").innerHTML = (" non sei autorizzato")
 }
